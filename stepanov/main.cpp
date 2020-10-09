1eafe74552bf7787f8e03ebd0c7abcc14fa17a0d
ben knowles <ben_junk@ionophore.com>
2020-10-09 12:22:47 -0500

    investigating std::vector

--- stepanov/main.cpp
+++ stepanov/main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
-
+#include <vector>
+#include <cstdio>
 
 int main() {
-	std::cout << "yay" << std::endl;
+	std::vector<int> v1(6);
+	std::vector<int> v2(123);
+
+	std::printf("v1:  size==%d; capacity==%d\n",v1.size(),v1.capacity());
+	std::printf("v2:  size==%d; capacity==%d\n",v2.size(),v2.capacity());
+
+	auto v3 = v2;
+	std::vector<int> v4(v2);
+	std::printf("v3:  size==%d; capacity==%d\n",v3.size(),v3.capacity());
+	std::printf("v4:  size==%d; capacity==%d\n",v4.size(),v4.capacity());
+	
+	v2 = v1;
+	std::printf("v2:  size==%d; capacity==%d\n",v2.size(),v2.capacity());
+
+	
+
+	//std::cout << "yay" << std::endl;
 
 	return 0;
 }
