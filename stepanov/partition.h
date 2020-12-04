5210175024f0a0a65b09e97ec8415e08a10b3503
ben knowles <ben_junk@ionophore.com>
2020-12-04 12:59:35 -0600

    partition() for fwd iterators and find_backward_if_not() for bidirectional iterators

--- stepanov/partition.h
+++ stepanov/partition.h
@@ -23,8 +23,32 @@ bool is_partitioned(I beg, I pvt, I end, P pred) {
 	return true;
 }
 
+template<typename I, typename P>  // I models fwd iterator
+I partition(I beg, I end, P pred) {
+	I pvt = std::find_if(beg,end,pred);
+	if (pvt == end) { return pvt; }
+	I curr = pvt;
+	++curr;
+	while (curr != end) {
+		if (!pred(*curr)) {
+			std::iter_swap(curr,pvt);
+			++pvt;
+		}
+		++curr;
+	}
+	return pvt;
+}
 
 
+template<typename I, typename P> // I models bidirectional iterator
+I find_backward_if_not(I beg, I end, P pred) {
+	if (end == beg) { return end; }
+	while (true) {
+		--end;
+		if (!pred(*end)) { return ++end; }
+		if (end == beg) { return end; }
+	}
+}
 
 
 };  // namespace nop
